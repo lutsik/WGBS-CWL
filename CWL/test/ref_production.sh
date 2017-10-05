fc3466bcacb142e354a89f19f5dd9f523e5ce1c3
KerstenBreuer <kersten-breuer@outlook.com>
2017-10-05 16:51:19 +0200

    input files dedined as File class not as string

--- CWL/test/ref_production.sh
+++ CWL/test/ref_production.sh
@@ -1,16 +1,11 @@
 SRC_DIR=`dirname $(readlink -f $0)`
 random_hash=`date | md5sum | head -c10`
+discription=$1
 
-TMP_DIR=${1}/test_run_${random_hash}
+TMP_DIR=/ngs_share/scratch/breuerk/test_cwl/${random_hash}_${discription}
 mkdir -p $TMP_DIR/out
 mkdir -p $TMP_DIR/work
 mkdir -p $TMP_DIR/base
 mkdir -p $TMP_DIR/cache
-cd $TMP_DIR
 
-
-cwltool --debug --tmpdir-prefix=`pwd`/work --basedir=`pwd`/base --outdir=`pwd`/out --cachedir=`pwd`/cache ${SRC_DIR}/../wgbs-workflow-production.yaml ${SRC_DIR}/../test/test-job-wfl-${2}.yml
-#cwltool --debug --tmpdir-prefix=`pwd`/work --basedir=`pwd`/base --outdir=`pwd`/out --cachedir=`pwd`/cache ${SRC_DIR}/wgbs-workflow-pilot.yaml ${SRC_DIR}/test/test-job-wfl.yml
-#cwltoil --logDebug --jobStore=`pwd`/jobstore --workDir=`pwd`/work --basedir=`pwd`/base --outdir=`pwd`/out ${SRC_DIR}/wgbs-workflow-pilot.yaml ${SRC_DIR}/test/test-job-wfl.yml
-#cwltool --debug --basedir=`pwd`  --tmpdir-prefix=`pwd` --cachedir=`pwd`/work --outdir=`pwd`/out ${SRC_DIR}/wgbs-workflow-pilot.yaml ${SRC_DIR}/test/test-job-wfl.yml
-
+cwltool --debug --tmpdir-prefix=$TMP_DIR/work --basedir=$TMP_DIR/base --outdir=$TMP_DIR/out --cachedir=$TMP_DIR/cache ../wgbs-workflow-production.yaml ./test-job-wfl-verySmallFile.yml
